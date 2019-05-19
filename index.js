9bdb10c1e08479b2ddae409756d6f5b671a0c2f2
Salvo Rinzivillo <rinzivillo@isti.cnr.it>
2019-05-19 22:39:28 +0200

    Installing and configuring apidocs and CORS

--- index.js
+++ index.js
@@ -5,10 +5,35 @@ const db = new sqlite3.Database('data/vc2008.db');
 
 // initialize express
 const express = require('express');
+const cors = require('cors');
 
 const api = express();
+api.use(cors());
 
 
+/**
+ * @api {get} /users Request list of users initiating a call
+ * @apiName GetUserIDList
+ * @apiGroup User
+ *
+ * @apiSuccess {Object[]} users List of objects with user IDs.
+ * @apiSuccess {Number} users.f User id initiating the call (f stands for 'from')
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     [
+ *       {"f": 0},
+ *       {"f": 1},
+ *       {"f": 2},
+ *     ]
+ *
+ * @apiError UserNotFound There is no user in the DB.
+ *
+ * @apiSampleRequest http://localhost:3000/users
+ *
+ *  @apiErrorExample {json} List error
+ *    HTTP/1.1 500 Internal Server Error
+ *
+ */
 // define a simple entry point to retrieve all users
 api.get('/users', (req, res) => {
   db.all('SELECT distinct f FROM callrecords', (err, rows) => {
