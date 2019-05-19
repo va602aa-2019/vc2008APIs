f50d2ed32e7af29ed865770d7d531bdcd1d6986f
Salvo Rinzivillo <rinzivillo@isti.cnr.it>
2019-05-19 23:12:46 +0200

    Endpoint to get list of calls for a user

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const cors = require('cors');
 const api = express();
 api.use(cors());
 
-
 /**
  * @api {get} /users Request list of users initiating a call
  * @apiName GetUserIDList
@@ -34,14 +33,58 @@ api.use(cors());
  *    HTTP/1.1 500 Internal Server Error
  *
  */
-// define a simple entry point to retrieve all users
 api.get('/users', (req, res) => {
   db.all('SELECT distinct f FROM callrecords', (err, rows) => {
     res.json(rows);
   });
 });
 
+/**
+ * @api {get} /user_calls/:id Request list of calls of a user
+ * @apiName GetUserCalls
+ * @apiGroup User
+ *
+ * @apiParam {id} id User id
+ *
+ * @apiSuccess {Object[]} calls List of calls of specified user.
+ * @apiSuccess {Number} users.f User id initiating the call (f stands for 'from')
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     [
+ *       {
+ *            "f": 1,
+ *            "t": 200,
+ *            "ts": null,
+ *           "duration": 1454,
+ *           "cid": 29
+ *       },
+ *        {
+ *            "f": 1,
+ *            "t": 2,
+ *            "ts": "2006-06-01 10:00:00",
+ *            "duration": 897,
+ *            "cid": 29
+ *        },
+ *     ]
+ *
+ * @apiError UserNotFound There is no user in the DB.
+ *
+ * @apiSampleRequest http://localhost:3000/user/:id
+ *
+ * @apiErrorExample {json} List error
+ *   HTTP/1.1 500 Internal Server Error
+ *
+ */
+api.get('/user/:id', (req, res) => {
+  const { id } = req.params;
+  db.all(`SELECT * FROM callrecords where f=${id}`, (err, rows) => {
+    res.json(rows);
+  });
+});
+
 
 // start listening
-api.listen(3000);
-console.log('Listening on port 3000...');
+
+const port = 3000;
+api.listen(port);
+console.log(`Listening on port ${port}...`);
